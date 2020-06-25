caeeaa2694ac6b73899576a6b736c051f1921405
sri kar <sri11321@gmail.com>
2020-06-25 17:25:02 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Platform,StyleSheet,Text,View } from "react-native";
+import PickerDemo from "./components/demopicker";
+export default class App extends Component{
+  render(){
+    return<PickerDemo></PickerDemo>
+  }
+}
---
+++ components/demopicker.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+
+import { View, Button,Platform, SafeAreaView } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import Modal from "react-native-modal";
+export  default class PickerDemo extends Component{
+    
+    state={
+        date:new Date('2020-06-25T15:00:00'),
+        mode:'date',
+        show:false
+    }
+    setDate=(event,date)=>{
+        date=date||this.state.state
+        this.setState({
+            show: Platform.OS==='ios'?true:false,
+            date,
+        });
+    }
+
+    show=mode=>{
+        this.setState({
+            show:true,
+            mode,
+        });
+    }
+    datepicker=()=>{
+        this.show('date');
+    }
+    timepicker=()=>{
+        this.show('time')
+    }
+
+
+  checkAndroidIos(show,date,mode){
+      if(Platform.OS==='ios'){
+          return(
+              <View></View>
+          )
+      }
+        else{
+          return(
+            <View>
+               
+            <View>
+              <Button onPress={this.datepicker} title="From date" />
+            </View>
+            <View>
+              <Button onPress={this.timepicker} title="From time" />
+            </View>
+            <View>
+              <Button onPress={this.datepicker} title="To date" />
+            </View>
+            <View>
+              <Button onPress={this.timepicker} title="To time" />
+            </View>
+            <View style={{height:300}}>
+
+      { show && <DateTimePicker
+               
+                value={date}
+                mode={mode}
+                is24Hour={true}
+                display="default"
+                onChange={this.setDate}>
+              </DateTimePicker>}
+              <Button title="Hide " onPress={this.showDatePicker}></Button>
+       </View>
+   
+
+
+</View>
+         
+          )
+      }
+  }
+
+    render(){
+        const {show,date,mode}=this.state;
+        return(
+           
+            <SafeAreaView>
+                {this.checkAndroidIos(show,date,mode)}
+            </SafeAreaView>
+        )
+        
+    }
+}
